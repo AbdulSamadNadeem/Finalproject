1ad1fd1f983f206758eee3c24c6e19910667f65a
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-07 18:29:10 +0500

    updated

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,26 +1,13 @@
 import { combineReducers, createStore } from "redux";
-import { AllProductsReducer ,  } from "./Reducers/AllProductsReducer"; 
+import { AllProductsReducer } from "./Reducers/AllProductsReducer";
 import { SelectedProductReducer } from "./Reducers/SelecetedProductReducer";
 import { AddToCartReducer } from "./Reducers/AddToCartreducer";
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  
-
-}
-
-const rootreducers = combineReducers({
-  All_Product_Reducer  : AllProductsReducer,
-  Selected_Product_Reducer : SelectedProductReducer,
-  CartReducer : AddToCartReducer
-})
-
-const persistedReducer = persistReducer(persistConfig, rootreducers)
-
-export let store = createStore(persistedReducer)
-export let persistor = persistStore(store)
+const rootReducers = combineReducers({
+  All_Product_Reducer: AllProductsReducer,
+  Selected_Product_Reducer: SelectedProductReducer,
+  CartReducer: AddToCartReducer,
+});
 
 
+export let store = createStore(rootReducers);
