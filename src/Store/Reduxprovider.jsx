ea1523b98b9d36f255c6dc0825b8f734742ccdbb
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-08 12:03:07 +0500

    Reduxprovider.jsx

--- src/Store/Reduxprovider.jsx
+++ src/Store/Reduxprovider.jsx
@@ -2,9 +2,14 @@
 
 import { Provider } from "react-redux";
 import { store } from "./Store";
-
+import PropTypes from "prop-types"; 
 const ReduxProvider = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
+};
+
+
+ReduxProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default ReduxProvider;
