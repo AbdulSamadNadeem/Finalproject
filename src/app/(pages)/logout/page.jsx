d34242ae9d59a8df462336694fe8137b4cf5389f
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-08 12:23:23 +0500

    Update page.jsx

--- src/app/(pages)/logout/page.jsx
+++ src/app/(pages)/logout/page.jsx
@@ -1,5 +1,4 @@
 "use client"
-import React from 'react'
 import { useNavigate } from 'react-router';
 import { auth, signOut} from '../../../Auth/Firebase';
 import { useRouter } from 'next/navigation';
