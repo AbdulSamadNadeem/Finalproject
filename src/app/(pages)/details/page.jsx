08d711c0d4dc117bd62b4b7f7876ce921649f748
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-08 12:22:23 +0500

    Update page.jsx

--- src/app/(pages)/details/page.jsx
+++ src/app/(pages)/details/page.jsx
@@ -1,5 +1,4 @@
 "use client"
-import React from "react";
 import Header from "../../Modules/Header/Header";
 import ProductDetail from "../../Modules/ProductDetail/ProductDetail";
 import Cards from "../../Modules/Cards/Cards";
